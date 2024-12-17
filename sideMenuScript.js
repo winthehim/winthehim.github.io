2c2307c8b60bf2e9b66fadc686d071217f752b7f
Win <winhim.c@gmail.com>
2024-12-17 22:18:47 +0000

    v0.16

---
+++ sideMenuScript.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySideMenu").style.width = "200px";
+    document.getElementById("main").style.marginRight = "200px";
+}
+
+function closeNav() {
+    document.getElementById("mySideMenu").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+} 
