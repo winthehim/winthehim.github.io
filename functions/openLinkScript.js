31ab1fc27291e44a4d19e499d900f8a109e9a0b3
Win <132519356+winthehim@users.noreply.github.com>
2024-12-19 23:42:08 +0000

    v1.0.0

---
+++ functions/openLinkScript.js
@@ -0,0 +1,7 @@
+function openLeftLink() {
+    window.open("https://www.youtube.com/watch?v=xvFZjo5PgG0", target="_blank");
+}
+
+function openRightLink() {
+    window.open("https://github.com/winthehim/winthehim.github.io", target="_blank");
+}
