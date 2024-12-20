91a54cb02a0ce302f33dd1c66862daf61f4d1a7b
Win <winhim.c@gmail.com>
2024-12-20 16:38:43 +0000

    v1.0.2

--- Old sidemenu code/sideMenuScript.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySideMenu").style.width = "200px";
-    document.getElementById("main").style.marginRight = "200px";
-}
-
-function closeNav() {
-    document.getElementById("mySideMenu").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
-} 
